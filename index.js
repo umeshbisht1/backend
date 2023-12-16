eb12434a9354812b3725f2bcac7b367dd62a8179
umesh bisht1 <umeshbisht792@gmail.com>
2023-12-16 12:28:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+res.send('umesh1.com')
+})
+app.get('/login',(req,res)=>{
+res.send('<h1> please login at chsai or code</h1>')
+})
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>jai ist dev</h2>')
+    
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// app.listen(port,()=>{
+
+// })
